b5f2e7081f50d5e39b51127338f18a8300426e70
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-01-18 22:00:57 -0500

    remove innecesary prop in BusinessModel

--- src/data/models/Business.js
+++ src/data/models/Business.js
@@ -2,12 +2,6 @@ const { DataTypes } = require("sequelize");
 const { db } = require("../../utils/database");
 
 const BusinessModel = db.define('Business', {
-    ID: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-    },
     nombre: {
         type: DataTypes.TEXT,
         allowNull: false
